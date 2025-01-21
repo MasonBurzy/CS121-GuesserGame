9442f73acbfeb77c192b8ea5879c3082d02ef4f2
MasonBurzy <mason.burzynski@bsu.edu>
2025-01-21 17:54:57 +0000

    no guesser game, 'Hello name'

--- guesser.c
+++ guesser.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+// start main
+
 int main(){
 	char name [20];
 	int correct ; 
@@ -11,6 +14,8 @@ int main(){
 	scanf("%s", name);
 	printf("Nice to mee you %s! Let's play a game. \n", name);
 
-}
+} 
+
+// end  main
 
 
